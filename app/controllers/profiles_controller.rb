f3f392877f4ca4edf0b1cd148a1237787c8b0f94
natGalynker <natalgal@gmail.com>
2016-09-30 09:08:41 -0400

    Fix exercise controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,61 +1,61 @@
-class ProfilesController < ProtectedController
-  # class ProfilesController < OpenReadController
-  before_action :set_profile, only: [:show, :update, :destroy]
-
-  # GET /profiles
-  # GET /profiles.json
-  def index
-    @profiles = current_user.profiles
-
-    render json: @profiles
-  end
-
-  # GET /profiles/1
-  # GET /profiles/1.json
-  def show
-    render json: @profile
-  end
-
-  # POST /profiles
-  # POST /profiles.json
-  def create
-    @profile = current_user.profiles.build(profile_params)
-
-    if @profile.save
-      render json: @profile, status: :created, location: @profile
-    else
-      render json: @profile.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /profiles/1
-  # PATCH/PUT /profiles/1.json
-  def update
-    # @profile = current_user.profiles.find(params[:id])
-
-    if @profile.update(profile_params)
-      head :no_content
-    else
-      render json: @profile.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /profiles/1
-  # DELETE /profiles/1.json
-  def destroy
-    @profile.destroy
-
-    head :no_content
-  end
-
-  private
-
-  def set_profile
-    @profile = Profile.find(params[:id])
-  end
-
-  def profile_params
-    params.require(:profile).permit(:username, :user_id, :location, :motivation,
-                                    :favorite_exercise)
-  end
-end
+# class ProfilesController < ProtectedController
+#   # class ProfilesController < OpenReadController
+#   before_action :set_profile, only: [:show, :update, :destroy]
+#
+#   # GET /profiles
+#   # GET /profiles.json
+#   def index
+#     @profiles = current_user.profiles
+#
+#     render json: @profiles
+#   end
+#
+#   # GET /profiles/1
+#   # GET /profiles/1.json
+#   def show
+#     render json: @profile
+#   end
+#
+#   # POST /profiles
+#   # POST /profiles.json
+#   def create
+#     @profile = current_user.profiles.build(profile_params)
+#
+#     if @profile.save
+#       render json: @profile, status: :created, location: @profile
+#     else
+#       render json: @profile.errors, status: :unprocessable_entity
+#     end
+#   end
+#
+#   # PATCH/PUT /profiles/1
+#   # PATCH/PUT /profiles/1.json
+#   def update
+#     # @profile = current_user.profiles.find(params[:id])
+#
+#     if @profile.update(profile_params)
+#       head :no_content
+#     else
+#       render json: @profile.errors, status: :unprocessable_entity
+#     end
+#   end
+#
+#   # DELETE /profiles/1
+#   # DELETE /profiles/1.json
+#   def destroy
+#     @profile.destroy
+#
+#     head :no_content
+#   end
+#
+#   private
+#
+#   def set_profile
+#     @profile = Profile.find(params[:id])
+#   end
+#
+#   def profile_params
+#     params.require(:profile).permit(:username, :user_id, :location, :motivation,
+#                                     :favorite_exercise)
+#   end
+# end
